86368057bedf4c9f0be0ed4c127f8c121792cf50
citizenlou <louis.sutton.iii@gmail.com>
2021-07-10 03:20:31 -0500

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+const Header = () => {
+    const [IsEnabled, setIsEnabled] = useState(false)
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="py-8">Our Pricing</h1>
+        <div className="flex space-x-6 justify-around items-stretch">
+          <span>Annually</span>
+          <Switch
+            checked={IsEnabled}
+            onChange={setIsEnabled}
+          >
+          <span className="block bg-white rounded-full shadow h-8 w-20">
+            <span className="block bg-red-700 shadow rounded-full h-full w-6/12"></span>
+          </span>
+          </Switch>
+          <span>Monthly</span>
+        </div>
+      </div>
+    </>
+    // Annual and Monthly div below
+  );
+};
+
+export default Header;
