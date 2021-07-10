a125340ba45c5158f2d3429ec078fd677f8235d5
citizenlou <louis.sutton.iii@gmail.com>
2021-07-10 15:02:28 -0500

    Changed tailwind config

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     const [IsEnabled, setIsEnabled] = useState(false)
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center text-">
         <h1 className="py-8">Our Pricing</h1>
         <div className="flex space-x-6 justify-around items-stretch">
           <span>Annually</span>
@@ -14,7 +14,7 @@ const Header = () => {
             checked={IsEnabled}
             onChange={setIsEnabled}
           >
-          <span className="block bg-white rounded-full shadow h-8 w-20">
+          <span className="block bg-purple-700 rounded-full shadow h-8 w-20">
             <span className="block bg-red-700 shadow rounded-full h-full w-6/12"></span>
           </span>
           </Switch>
